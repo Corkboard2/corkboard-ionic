94744718fdbebe76c34e16a26853a7ad86060dc5
Sartaj Dua <duass@uci.edu>
2023-02-22 20:21:00 -0800

    added layour for app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  ratingsSubmitted = false;
+  ratings: number[] = [0, 0, 0, 0 ,0];
+  dishes = ['mexican_dish.jpeg', 'greek_dish.avif', 'pizza_dish.webp', 'salad_dish.webp', 'sandwich_dish.webp']
+  allRated = false;
+  constructor() {
+  }
+  ngOnInit() {
+  }
+  rateDish(index: number, rating: number) {
+    this.ratings[index] = rating;
+    this.allRated = true;
+    this.ratings.forEach((rating) => {
+      if(rating == 0) {
+        this.allRated = false;
+      }
+    });
+    console.log(this.ratings)
+  }
+  getStarColor(index: number, starNumber: number) {
+    if(starNumber <= this.ratings[index]) {
+      return "yellow";
+    }
+    return "grey"
+  }
+  submitRatings() {
+    this.ratingsSubmitted = true;
+  }
 }
